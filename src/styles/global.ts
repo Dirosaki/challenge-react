e52d6138ec66d34ad644ca2a295b40e676f36416
Dirosaki <diegosgomes27@gmail.com>
2022-09-03 18:47:44 -0300

    Global style refactoring

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,29 +2,28 @@ import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 	${({ theme }) => css`
-    *,
-    *::before,
-    *::after {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      outline: none;
-    }
+		*,
+		*::before,
+		*::after {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
 
-    body {
-      background: ${theme.colors.background};
-      color: ${theme.colors.text};
-      -webkit-font-smoothing: antialiased;
-    }
+		body {
+			background: ${theme.colors.background};
+			color: ${theme.colors.text};
+			-webkit-font-smoothing: antialiased;
+		}
 
-    body,
-    input,
-    textarea,
-    select,
-    button {
-      font-family: 'Open Sans', sans-serif;
-      font-size: 1rem;
-      font-weight: 400;
-    }
-  `}
+		body,
+		input,
+		textarea,
+		select,
+		button {
+			font-family: 'Open Sans', sans-serif;
+			font-size: 1rem;
+			font-weight: 400;
+		}
+	`}
 `
