8884bdeb33f0bfe214777d0b333301514eba777a
Dirosaki <diegosgomes27@gmail.com>
2022-09-03 02:47:05 -0300

    Default layout configuration

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { DefaultLayout } from 'layouts/DefaultLayout'
+
 export function Router() {
 	return (
 		<Routes>
-			<Route path="/" element={<h1>Home</h1>} />
-			<Route path="/carrinho" element={<h1>Carrinho</h1>} />
-			<Route path="/compra-realizada" element={<h1>Compra Realizada</h1>} />
+			<Route element={<DefaultLayout />}>
+				<Route index element={<h1>Home</h1>} />
+				<Route path="/carrinho" element={<h1>Carrinho</h1>} />
+				<Route path="/compra-realizada" element={<h1>Compra Realizada</h1>} />
+			</Route>
 		</Routes>
 	)
 }
